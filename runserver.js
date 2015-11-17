a28f39b1b661549f7977faf46b82b27f593bc1f9
Mathias Quintero <mathias.quintero@tum.de>
2015-11-17 02:38:44 +0100

    Added Api For Basic User Info

--- runserver.js
+++ runserver.js
@@ -6,15 +6,17 @@ var http = require('http');
 var dispatcher = require('httpdispatcher');
 var url = require('url');
 var ObjectId = require('mongodb').ObjectID;
-var localtunnel = require('localtunnel');
 
-var tunnel = localtunnel(8080,{"port":8080, "subdomain": "movies"}, function(err, tunnel) {
-    if (err === null ) {
-      console.log("New Tunnel Assigned!: " + tunnel.url);
-    } else {
-      console.log("Error:  " + err);
-    }
-});
+// var localtunnel = require('localtunnel');
+//
+//
+// var tunnel = localtunnel(8080,{"port":8080, "subdomain": "movies"}, function(err, tunnel) {
+//     if (err === null ) {
+//       console.log("New Tunnel Assigned!: " + tunnel.url);
+//     } else {
+//       console.log("Error:  " + err);
+//     }
+// });
 
 //Mongo DB Client
 
@@ -22,9 +24,11 @@ var MongoClient = require('mongodb').MongoClient;
 
 var mongoURL = 'mongodb://localhost:27017/test';
 
+var port = process.env.PORT || 8080;
+
 http.createServer(function (request, response) {
   dispatcher.dispatch(request, response);
-}).listen(8080);
+}).listen(port);
 
 var isEmpty = function(value){
     return Boolean(value && typeof value == 'object') && !Object.keys(value).length;
@@ -43,6 +47,25 @@ var isUserRegistered = function(id, callback) {
     });
   });
 };
+
+var rewriteAttributeForUser = function(id, callback, res) {
+  MongoClient.connect(mongoURL, function(err, db) {
+    db.collection('users').findOne({"facebookID" : id}, function(error, doc) {
+      if (isEmpty(doc) || doc === null || err !== null) {
+        db.close();
+        res.writeHead(404, {'Content-Type': 'application/json'});
+        res.end("Wasn't able to rewrite it. Perhaps there is no user with that id");
+      } else {
+        var newUser = callback(doc);
+        db.collection('users').save(newUser);
+        db.close();
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify(newUser,0,4));
+      }
+    });
+  });
+};
+
 
 var registerUser = function(user, callback) {
   MongoClient.connect(mongoURL, function(err, db) {
@@ -126,22 +149,68 @@ dispatcher.onGet("/watchlist", function(req, res) {
   isUserRegistered(id, callback);
 });
 
-dispatcher.onPost("/watchlist", function(req, res) {
+dispatcher.onPost("/language", function(req, res) {
   var queryObject = url.parse(req.url, true);
   var query = queryObject.query;
   var id = query.userid;
-  var movie = query.movie;
-  var callback = function(data) {
-    if (data !== null) {
-      var tickets = data.tickets;
-      res.writeHead(200, {'Content-Type': 'application/json'});
-      res.end(JSON.stringify(tickets,0,4));
-    } else {
-      res.writeHead(404, {'Content-Type': 'application/json'});
-      res.end("No Tickets Available For User");
-    }
-  };
-  isUserRegistered(id, callback);
+  try {
+    var pref = query.pref;
+    rewriteAttributeForUser(id, function(user){
+      user.preferredLanguageSetting = pref;
+      return user;
+    },res);
+  } catch (e) {
+    res.writeHead(501, {'Content-Type': 'application/json'});
+    res.end("You need to specify what setting to add through the query.");
+  }
+});
+
+dispatcher.onPost("/distance", function(req, res) {
+  var queryObject = url.parse(req.url, true);
+  var query = queryObject.query;
+  var id = query.userid;
+  try {
+    var pref = query.pref;
+    rewriteAttributeForUser(id, function(user){
+      user.maxDistanceForCinema = pref;
+      return user;
+    },res);
+  } catch (e) {
+    res.writeHead(501, {'Content-Type': 'application/json'});
+    res.end("You need to specify what setting to add through the query.");
+  }
+});
+
+dispatcher.onPost("/notifyWatch", function(req, res) {
+  var queryObject = url.parse(req.url, true);
+  var query = queryObject.query;
+  var id = query.userid;
+  try {
+    var pref = query.pref !== "0";
+    rewriteAttributeForUser(id, function(user){
+      user.notifyOnWatchList = pref;
+      return user;
+    },res);
+  } catch (e) {
+    res.writeHead(501, {'Content-Type': 'application/json'});
+    res.end("You need to specify what setting to add through the query.");
+  }
+});
+
+dispatcher.onPost("/notifySub", function(req, res) {
+  var queryObject = url.parse(req.url, true);
+  var query = queryObject.query;
+  var id = query.userid;
+  try {
+    var pref = query.pref !== "0";
+    rewriteAttributeForUser(id, function(user){
+      user.notifyOnSubscription = pref;
+      return user;
+    },res);
+  } catch (e) {
+    res.writeHead(501, {'Content-Type': 'application/json'});
+    res.end("You need to specify what setting to add through the query.");
+  }
 });
 
 console.log('Server running at http://127.0.0.1:80/');
