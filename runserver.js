de10e422ef3dbf8e5c293baa50598dd89b92a236
Mathias Quintero <mathias.quintero@tum.de>
2015-11-19 18:39:38 +0100

    Changing Allow Origin

--- runserver.js
+++ runserver.js
@@ -32,7 +32,7 @@ http.createServer(function (request, response) {
 
 var respondWith = function(res, item) {
   res.writeHead(200, {'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': 'http://awesomesourcecode.github.io/Movies-Web-Frontend/',
+    'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'GET, POST',
     'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
   });
